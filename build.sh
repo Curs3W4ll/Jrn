55ba71972bd9ff842563fc855a29e5fcf998d2b2
Corentin Hervaud <chervaud@dydu.ai>
2021-07-27 15:08:06 +0200

    {FILES} [Modif] Update build and remove scripts

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-chmod +x ./jrn
+echo "Make remove script executable..." &> /dev/null
 chmod +x ./remove.sh
+echo "Make binary executable..." &> /dev/null
+chmod +x ./jrn
+echo "Copying binary executable..." &> /dev/null
 sudo cp ./jrn /bin/jrn
--- remove.sh
+++ remove.sh
@@ -1,2 +1,9 @@
-sudo rm /bin/jrn
-rm -rf ./* .gitignore .git
+#!/bin/sh
+echo "Removing binary..."
+sudo rm /bin/jrn &> /dev/null
+echo "Removing project..."
+rm -rf ./* .gitignore .git &> /dev/null
+echo "Removing journey file..."
+rm -rf $HOME/Documents/my_journey.txt &> /dev/null
+echo "Removing journey settings file..."
+rm -rf $HOME/.jrn.settings &> /dev/null
